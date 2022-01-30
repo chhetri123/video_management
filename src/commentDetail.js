0af94554c129219def88a666f83756fdaadf2a0a
chhetri123 <chhetridon123@gmail.com>
2022-01-31 00:47:32 +0545

    learning react component

---
+++ src/commentDetail.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const CommentDetail = (prop) => {
+  console.log(prop);
+  return (
+    <>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img
+            alt="avatar"
+            src="https://thumbs.dreamstime.com/b/happy-smiling-geek-hipster-beard-man-cool-avatar-geek-man-avatar-104871313.jpg"
+          />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            {prop.author}
+          </a>
+
+          <div className="metadata">
+            <span className="date">{prop.date}</span>
+          </div>
+          <div className="Text"> {prop.comment}</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./commentDetail";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// class App extends React.Component {}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// create a react Components
+// Its is a function or class that produces HTMl to show the user and handles feedback from users
+
+/*  
+Component Nesting -> A component can be shown inside of another 
+Conponsnt Resuability -> We want to make components tat can be easily reused through out application 
+component Configuration -> we should ba able to configure a components when it is created 
+
+
+//  Props -> system for passing data from a parent component to a cild component -> Goal is o customize or configure a child component 
+*/
+
+// const getClickMe = () => {
+//   return "Mansh chhetri";
+// };
+// const App = function () {
+//   return (
+//     <>
+//       <div className="chhetri" style={{ backgroundColor: "Red" }}>
+//         {/* {" " */}
+//         Manish chhetri
+//       </div>
+//       <div
+//         style={{
+//           border: "10px solid yellow",
+//           height: "200px",
+//           width: "200px",
+//           borderRadius: "3px",
+//           backgroundColor: "red",
+//         }}
+//       >
+//         {getClickMe()}
+//       </div>
+//     </>
+//   );
+// };
+
+const App = () => {
+  return (
+    <>
+      <div className="ui container comments" style={{ fontSize: "35px" }}>
+        <CommentDetail
+          author="Manish Chhetri"
+          comment="Its is nice"
+          date={"Today 9:30 pm"}
+        />
+        <CommentDetail
+          author="Alex Chhetri"
+          comment="very good"
+          date={"yesterday 11:20pm"}
+        />
+        <CommentDetail
+          author="Mandip  poudel"
+          comment="I like it"
+          date={"Just now "}
+        />
+      </div>
+    </>
+  );
+};
+ReactDOM.render(<App />, document.getElementById("root"));
