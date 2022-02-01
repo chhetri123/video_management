372fea84438b79f07bbf0b049171da17c131b46d
chhetri123 <chhetridon123@gmail.com>
2022-02-01 14:43:01 +0545

    State in React Component

--- src/index.js
+++ src/index.js
@@ -1,76 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CommentDetail from "./CommentDetail";
-import ApprovalCard from "./ApprovalCard";
+import SeasonDisplay from "./SeasonDispaly";
 
-// class App extends React.Component {}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, lng: null, errMessage: null };
 
-// create a react Components
-// Its is a function or class that produces HTMl to show the user and handles feedback from users
-
-/*  
-Component Nesting -> A component can be shown inside of another 
-Conponsnt Resuability -> We want to make components tat can be easily reused through out application 
-component Configuration -> we should ba able to configure a components when it is created 
-
-
-//  Props -> system for passing data from a parent component to a cild component -> Goal is o customize or configure a child component 
-*/
-
-// const getClickMe = () => {
-//   return "Mansh chhetri";
-// };
-// const App = function () {
-//   return (
-//     <>
-//       <div className="chhetri" style={{ backgroundColor: "Red" }}>
-//         {/* {" " */}
-//         Manish chhetri
-//       </div>
-//       <div
-//         style={{
-//           border: "10px solid yellow",
-//           height: "200px",
-//           width: "200px",
-//           borderRadius: "3px",
-//           backgroundColor: "red",
-//         }}
-//       >
-//         {getClickMe()}
-//       </div>
-//     </>
-//   );
-// };
-
-const App = () => {
-  return (
-    <>
-      <div className="ui container comments" style={{ fontSize: "25px" }}>
-        {/* passing child component  to a parent component*/}
-        <ApprovalCard>
-          <CommentDetail
-            author="Manish Chhetri" // Property name and value
-            comment="Its is nice"
-            date={"Today 9:30 pm"}
-          />
-        </ApprovalCard>
-        <ApprovalCard>
-          <CommentDetail
-            author="Alex Chhetri"
-            comment="very good"
-            date={"yesterday 11:20pm"}
-          />
-        </ApprovalCard>
-        <ApprovalCard>
-          <CommentDetail
-            author="Mandip  poudel"
-            comment="I like it"
-            date={"Just now "}
-          />
-        </ApprovalCard>
-      </div>
-    </>
-  );
+    window.navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        this.setState({ lat: pos.coords.latitude, lng: pos.coords.longitude });
+      },
+      (err) => this.setState({ errMessage: err.message })
+    );
+  }
+  render() {
+    if (this.state.errMessage && !this.state.lat) {
+      return <h1>Error:{this.state.errMessage}</h1>;
+    }
+    if (!this.state.errMessage && !this.state.lat) {
+      return <h1>Loading...</h1>;
+    } else {
+      return <h1>Coordinates:{`${this.state.lat},${this.state.lng}`}</h1>;
+    }
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
