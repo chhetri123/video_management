7ce7ff710de4b410a36cad14f83f8f9ed2b0c99c
chhetri123 <chhetridon123@gmail.com>
2022-02-13 21:24:41 +0545

    adding API key

--- src/APIs/youtube.js
+++ src/APIs/youtube.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const API_KEY = "AIzaSyAjaB9AzQYAQpDgmJSNRQLTe7GzLC4U8xE";
+// const API_KEY = "AIzaSyAjaB9AzQYAQpDgmJSNRQLTe7GzLC4U8xE";
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
     part: "snippet",
     maxResults: 30,
-    key: API_KEY,
+    key: "AIzaSyAjaB9AzQYAQpDgmJSNRQLTe7GzLC4U8xE",
   },
 });
